a9c757a064a99d6cfc745f450ee3985889022d80
artemats <artem.matseruk@gmail.com>
2020-10-27 15:07:12 +0200

    add moving links

--- src/js/renderers/home.js
+++ src/js/renderers/home.js
@@ -1,10 +1,12 @@
 import Highway from '@dogstudio/highway';
 import {horizontalMoving} from "../animations/horizontalTitleMove";
+import {mouseFollowLink} from "../animations/mouseFollowLink";
 
 class HomeRenderer extends Highway.Renderer {
 
     onEnter() {
         horizontalMoving();
+        mouseFollowLink();
         // console.log('home page, on enter');
     }
 
