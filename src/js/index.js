a71f56de111e42d33a85e93c00e5fb2237d18dfb
artemats <artem.matseruk@gmail.com>
2020-11-09 10:24:58 +0200

    add lottie

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,22 @@
 import '../sass/styles.scss';
 import './smooth-scroll/smoothScroll';
-import {followMoving} from "./animation/follow-moving";
+import { followMoving } from "./animation/follow-moving";
+import lottie  from 'lottie-web';
 
 /*
 Initial follow moving for links
  */
 followMoving();
+
+/*
+Initial lottie animation
+ */
+lottie.loadAnimation({
+   container: document.querySelector('.follow-link-bg'),
+   renderer: 'svg',
+   loop: true,
+   autoplay: true,
+   path: '../lottie/lightning/data.json'
+});
 
 console.log('ready');
